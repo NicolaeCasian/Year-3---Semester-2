1cbdeca0b00ba03b3eb6dd4344bf648e30979fd0
NicolaeCasian <136645209+NicolaeCasian@users.noreply.github.com>
2025-03-25 15:53:38 +0000

    7 tests added to unit testing

--- Semester2.1/Network/Assignment_1/rpc_unittests.py
+++ Semester2.1/Network/Assignment_1/rpc_unittests.py
@@ -24,8 +24,36 @@ class MyTests(unittest.TestCase):
         response = requests.post("http://localhost:5001/", json=request("delete_folder", {"foldername": "test"}))
         parsed = parse(response.json())
         print("result from server: " + parsed.result)
-        self.assertEqual(parsed.result, 'Folder '"test"' deleted.')
+        self.assertEqual(parsed.result, 'Folder test deleted.')
 
+    #Test 4: Test the whoareyou function
+    def test_whoareyou(self):
+        response = requests.post("http://localhost:5001/", json=request("whoareyou"))
+        parsed = parse(response.json())
+        print("result from server: " + parsed.result)
+        self.assertEqual(parsed.result, 'Server 1 running on port 5001')
+
+    #Test 5: Test the get_version function
+    def test_get_version(self):
+        response = requests.post("http://localhost:5001/", json=request("get_version"))
+        parsed = parse(response.json())
+        print("result from server: " + parsed.result)
+        self.assertEqual(parsed.result, '3.13.2')
+    
+    #Test 6: Test the get_version function
+    def test_search(self):
+        response = requests.post("http://localhost:5001/", json=request("search", {"filename": "s.py"}))
+        parsed = parse(response.json())
+        print("result from server: " + parsed.result)
+        self.assertEqual(parsed.result, "File 's.py' found.")
+    
+    #Test 7: Test the startup function
+    def test_search(self):
+        response = requests.post("http://localhost:5001/", json=request("startup", {"x": "2"}))
+        parsed = parse(response.json())
+        print("result from server: " + parsed.result)
+        self.assertEqual(parsed.result, "Started server 2 on port 5002.")
+    
     
 if __name__ == '__main__':
 
